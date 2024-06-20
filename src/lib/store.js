b4b27066de421486b7dce13e6e99cf58d83f5026
AnkushDX <ankushpanwar.dx@gmail.com>
2024-06-20 18:13:51 +0530

    ankush code

--- src/lib/store.js
+++ src/lib/store.js
@@ -1011,8 +1011,8 @@ export async function getCreditNotes(key, retailer, manufacturer) {
   if (data.status == 300) {
     DestoryAuth()
   } else {
-    console.log({dataResult : data?.result})
-    return data?.result
+    console.log({dataResult : data})
+    return data
   }
 }
 
